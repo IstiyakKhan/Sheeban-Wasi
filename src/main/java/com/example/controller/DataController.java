8016bb1e220dd227a9d578eb5dbe868f7526e02b
Istiyak Khan <76749265+IstiyakKhan@users.noreply.github.com>
2023-07-15 15:10:56 +0530

    .

--- src/main/java/com/example/controller/DataController.java
+++ src/main/java/com/example/controller/DataController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class DataController {
 
     @Autowired

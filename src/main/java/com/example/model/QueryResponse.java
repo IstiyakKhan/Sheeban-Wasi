abdaccec6d3228d000966c70575f7c31e6eb30fa
Istiyak Khan <76749265+IstiyakKhan@users.noreply.github.com>
2023-07-15 17:02:15 +0530

    minor

--- src/main/java/com/example/model/QueryResponse.java
+++ src/main/java/com/example/model/QueryResponse.java
@@ -5,19 +5,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class QueryResponse {
 
-    private String column;
 
     private String data;
 
     private int row;
-
-    public String getColumn() {
-        return column;
-    }
-
-    public void setColumn(String column) {
-        this.column = column;
-    }
 
     public String getData() {
         return data;
